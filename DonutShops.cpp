8d65b1d3b070bbaf55ff93ef1c06b37df2537a8b
Poornima Tewatia <poornimatewatia48@gmail.com>
2026-07-07 16:42:44 +0530

    today's commit

---
+++ DonutShops.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+pair<int,int> solve(int a,int b,int c){
+
+}
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+   int a,b,c;
+   cin>>a>>b>>c;
+  pair<int,int>p=solve(a,b,c);
+  cout<<p.first<<" "<<p.second<<endl;
+  }
+  return 0;
+}
--- Game23.cpp
+++ Game23.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
 using namespace std;
+int solve(long long n,long long m){
+  if(n==m)return 0;
+  if(m%n!=0)return -1;
+   long long need=m/n;
+   int cnt=0;
+   while(need%2==0&&need>0){
+    need/=2;
+    cnt++;
+   }
+   if(need==1)return cnt;
+   while(need%3==0&&need>0){
+    need/=3;
+    cnt++;
+   }
+   if(need==1)return cnt;
+   
+   return -1;
+}
+
 int main(){
-  int n,m;
+  long long n,m;
   cin>>n>>m;
-  
+  cout<<solve(n,m)<<endl;
   return 0;
 }
